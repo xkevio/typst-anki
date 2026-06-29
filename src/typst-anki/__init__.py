6f483d6377b7862971d9059b2c6d8e343b972366
xkevio <xkeviota@gmail.com>
2026-06-29 13:21:13 +0200

    fix: replace <p> tag and mention cursor in README

--- src/typst-anki/__init__.py
+++ src/typst-anki/__init__.py
@@ -27,7 +27,7 @@ import typst
 
 config = mw.addonManager.getConfig(__name__) or {
     "render-type": "mathml",
-    "preamble": "user_files/preamble.typ"
+    "preamble": "user_files/preamble.typ",
 }
 preamble = Path(os.path.join(os.path.dirname(__file__), config["preamble"])).read_text()
 
@@ -64,7 +64,7 @@ def gen_typst_math(typst_math: str, render_type: Export, display_math: bool) -> 
         )
 
         return (
-            output.decode()
+            output.decode().replace("<p>", "").replace("</p>", "")
             if render_type == Export.MATHML
             else svg_to_base64_img(output, display_math)
         )
@@ -121,7 +121,7 @@ def typst_editor(editor: Editor, display_math=False):
         showInfo("Web view of editor could not be initialized!")
         return
 
-    input_dialog = TypstInputDialog(display_math=display_math, config = config)
+    input_dialog = TypstInputDialog(display_math=display_math, config=config)
     input_dialog.input.setFocus()
     input_dialog.button.setDefault(True)
 
